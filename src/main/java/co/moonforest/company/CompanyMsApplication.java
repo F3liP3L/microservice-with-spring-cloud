bf1a260123a21e88147bc54c2afb0ab692849b20
Juan Cardona <felipe7u7432@gmail.com>
2024-09-06 18:31:04 -0500

    :sparkles: feat: Add connection to registry-server microservice

--- src/main/java/co/moonforest/company/CompanyMsApplication.java
+++ src/main/java/co/moonforest/company/CompanyMsApplication.java
@@ -2,12 +2,12 @@ package co.moonforest.company;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CompanyMsApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CompanyMsApplication.class, args);
     }
-
 }
